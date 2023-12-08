2e9473ce3a410f77ca559a4595d72a6e5da30be0
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-12-08 21:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function changeColor() {
+            var blockId = document.getElementById("block_id").value;
+            var colorId = document.getElementById("colour_id").value;
+            var blockElement = document.getElementById(blockId);
+
+            if (blockElement) {
+                // Set all grid items' background to transparent
+                var gridItems = document.querySelectorAll(".grid-item");
+                gridItems.forEach(function(item) {
+                    item.style.backgroundColor = "transparent";
+                });
+
+                // Change the background color of the selected grid item
+                blockElement.style.backgroundColor = colorId;
+            } else {
+                alert("Invalid Block ID");
+            }
+        }
+
+        function resetColors() {
+            // Set all grid items' background to transparent
+            var gridItems = document.querySelectorAll(".grid-item");
+            gridItems.forEach(function(item) {
+                item.style.backgroundColor = "transparent";
+            });
+        }
